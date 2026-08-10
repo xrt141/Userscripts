8658211ab175b475a9343ed86a49b1942cddd3ce
xrt141 <xrt141@gmail.com>
2026-08-10 15:36:35 -0400

    Less sucky color change

--- Luminance Tag Highlighter+.user.js
+++ Luminance Tag Highlighter+.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Luminance Tag Highlighter+
 // @namespace    http://tampermonkey.net/
-// @version      2.8.2
+// @version      2.8.3
 // @description  Branched from Emp++ Tag Highlighter v0.7.9b
 // @author       xrt141, allebady
 // @grant        GM_getValue
